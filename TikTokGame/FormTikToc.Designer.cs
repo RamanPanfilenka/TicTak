963712ac9006a26130aa7addcd2289ff045f62e0
RomanPanfilenko <47428169+RomanPanfilenko@users.noreply.github.com>
2019-02-22 18:16:54 +0300

    Добавьте файлы проекта.

---
+++ TikTokGame/FormTikToc.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace TikTokGame
+{
+    partial class FormTikToc
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTikToc));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.начатьИгруToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сбросToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 135);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 135);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(166, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 135);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(166, 189);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(148, 135);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(166, 330);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(148, 135);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(320, 48);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(148, 135);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(320, 189);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(148, 135);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(320, 330);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(148, 135);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 135);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(485, 28);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.начатьИгруToolStripMenuItem,
+            this.сбросToolStripMenuItem});
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // начатьИгруToolStripMenuItem
+            // 
+            this.начатьИгруToolStripMenuItem.Name = "начатьИгруToolStripMenuItem";
+            this.начатьИгруToolStripMenuItem.Size = new System.Drawing.Size(259, 26);
+            this.начатьИгруToolStripMenuItem.Text = "Начать новую серию игр";
+            this.начатьИгруToolStripMenuItem.Click += new System.EventHandler(this.начатьИгруToolStripMenuItem_Click);
+            // 
+            // сбросToolStripMenuItem
+            // 
+            this.сбросToolStripMenuItem.Name = "сбросToolStripMenuItem";
+            this.сбросToolStripMenuItem.Size = new System.Drawing.Size(259, 26);
+            this.сбросToolStripMenuItem.Text = "Сброс";
+            this.сбросToolStripMenuItem.Click += new System.EventHandler(this.сбросToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "X:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "O:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(317, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "0";
+            // 
+            // FormTikToc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 469);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormTikToc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem начатьИгруToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сбросToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TikTokGame/FormTikToc.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikTokGame
+{
+    public partial class FormTikToc : Form
+    {
+        private int whoMove = 1;
+        private int[] moved = new int[9];
+        public FormTikToc()
+        {
+            InitializeComponent();
+        }
+
+        private void WhoWin()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 0)
+                {
+                    if (moved[i] == 1 && moved[i + 1] == 1 && moved[i + 2] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+
+                    if (moved[i] == 1 && moved[i + 3] == 1 && moved[i + 6] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+
+                    if (moved[i] == 1 && moved[i + 4] == 1 && moved[i + 8] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 1)
+                {
+                    if (moved[i] == 1 && moved[i + 3] == 1 && moved[i + 6] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 2)
+                {
+                    if (moved[i] == 1 && moved[i + 3] == 1 && moved[i + 6] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+
+                    if (moved[i] == 1 && moved[i + 2] == 1 && moved[i + 4] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 3)
+                {
+                    if (moved[i] == 1 && moved[i + 1] == 1 && moved[i + 2] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 6)
+                {
+                    if (moved[i] == 1 && moved[i + 1] == 1 && moved[i + 2] == 1)
+                    {
+                        MessageBox.Show("Победили крестики", "Поздравляем");
+                        label3.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 0)
+                {
+                    if (moved[i] == 2 && moved[i + 1] == 2 && moved[i + 2] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+
+                    if (moved[i] == 2 && moved[i + 3] == 2 && moved[i + 6] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+
+                    if (moved[i] == 2 && moved[i + 4] == 2 && moved[i + 8] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 1)
+                {
+                    if (moved[i] == 2 && moved[i + 3] == 2 && moved[i + 6] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 2)
+                {
+                    if (moved[i] == 2 && moved[i + 3] == 2 && moved[i + 6] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+
+                    if (moved[i] == 2 && moved[i + 2] == 2 && moved[i + 4] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 3)
+                {
+                    if (moved[i] == 2 && moved[i + 1] == 2 && moved[i + 2] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+                if (i == 6)
+                {
+                    if (moved[i] == 2 && moved[i + 1] == 2 && moved[i + 2] == 2)
+                    {
+                        MessageBox.Show("Победили нолики", "Поздравляем");
+                        label4.Text = (int.Parse(label3.Text) + 1).ToString();
+                        Reset();
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button9.Text = "X";
+                    moved[8] = 1;
+                }
+                else
+                {
+                    button9.Text = "O";
+                    moved[8] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (button1.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button1.Text = "X";
+                    moved[0] = 1;
+                }
+                else
+                {
+                    button1.Text = "O";
+                    moved[0] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void changeMovedPlayer()
+        {
+            whoMove = -whoMove;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button4.Text = "X";
+                    moved[3] = 1;
+                }
+                else
+                {
+                    button4.Text = "O";
+                    moved[3] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button7.Text = "X";
+                    moved[6] = 1;
+                }
+                else
+                {
+                    button7.Text = "O";
+                    moved[6] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button2.Text = "X";
+                    moved[1] = 1;
+                }
+                else
+                {
+                    button2.Text = "O";
+                    moved[1] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button3.Text = "X";
+                    moved[2] = 1;
+                }
+                else
+                {
+                    button3.Text = "O";
+                    moved[2] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button6.Text = "X";
+                    moved[5] = 1;
+                }
+                else
+                {
+                    button6.Text = "O";
+                    moved[5] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button8.Text = "X";
+                    moved[7] = 1;
+                }
+
+                else
+                {
+                    button8.Text = "O";
+                    moved[7] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "")
+            {
+                if (whoMove == 1)
+                {
+                    button5.Text = "X";
+                    moved[4] = 1;
+                }
+                else
+                {
+                    button5.Text = "O";
+                    moved[4] = 2;
+                }
+                changeMovedPlayer();
+                WhoWin();
+            }
+            else
+            {
+                MessageBox.Show("Ути-пути, сюда незя", "Ошибка Хода");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void начатьИгруToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label3.Text = "0";
+            label4.Text = "0";
+            Reset();
+        }
+
+        private void Reset()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            whoMove = 1;
+            for (int i = 0; i < 9; i++)
+            {
+                moved[i] = 0;
+            }
+        }
+
+        private void сбросToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
